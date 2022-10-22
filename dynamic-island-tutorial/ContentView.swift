d71e42ba7a0e1a2f47055305b9092c91873af304
pjt3591oo <pjt3591oo@gmail.com>
2022-10-22 11:00:32 +0900

    Initial Commit

---
+++ dynamic-island-tutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  dynamic-island-tutorial
+//
+//  Created by 박정태 on 2022/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dynamic-island-tutorial/dynamic_island_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamic_island_tutorialApp.swift
+//  dynamic-island-tutorial
+//
+//  Created by 박정태 on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct dynamic_island_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
